aeb65ae0b2eee6655bf85a6c4daa4d9d60677f91
melina <melina@melina-PC>
2018-05-25 21:44:25 -0300

    original

--- CasaDeApuestas/src/Historial/HistorialDePartido.java
+++ CasaDeApuestas/src/Historial/HistorialDePartido.java
@@ -4,6 +4,8 @@ public class HistorialDePartido {
 
 	private String oponenteUno;
 	private String oponenteDos;
+	private Integer partidosGanadosOponenteUno;
+	private Integer partidosGanadosOponenteDos;
 	
 	public HistorialDePartido(String oponenteUno,String oponenteDos){
 		
@@ -12,14 +14,20 @@ public class HistorialDePartido {
 		
 	}
 
-	private void setOponenteDos(String oponenteDos2) {
+	public void setOponenteDos(String oponenteDos2) {
 	 
 		this.oponenteDos= oponenteDos2;
 		
 	}
 
-	private void setOponenteUno(String oponenteUno1) {
+	public void setOponenteUno(String oponenteUno1) {
 		
 		this.oponenteUno= oponenteUno1;
+	}
+	
+	public void ganoOponenteUno(){
+		
+		this.partidosGanadosOponenteUno= partidosGanadosOponenteUno + 1;
+		
 	}
 }
--- CasaDeApuestas/test/testCuotaDeApuesta/TestCuotaDeApuesta.java
+++ CasaDeApuestas/test/testCuotaDeApuesta/TestCuotaDeApuesta.java
@@ -9,10 +9,11 @@ public class TestCuotaEventoDeportivo {
 		
 		Deporte futbol     = new Deporte("futbol");
 		HistorialDePartido historial = new Historial("Boca","River");
-		historial.ganoOponenteUno("Boca");
+		historial.ganoOponenteUno();
 		Partido partido    = new Partido(futbol,["Boca","River"],"21:00,14mayo","Nuñez","ganoBoca");
 		
 		assertEquals(historial.getGanoOponenteUno(),1);
 	}
 
+	
 }
