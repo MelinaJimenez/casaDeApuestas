dcb0077074c4afa0890ddc14a28416c5cec7dc5d
melina <melina@melina-PC>
2018-05-26 03:19:17 -0300

    original

--- CasaDeApuestas/src/cuota/CompetenciaHistoricaDirectaStrategy.java
+++ CasaDeApuestas/src/cuota/CompetenciaHistoricaDirectaStrategy.java
@@ -3,6 +3,9 @@ package cuota;
 public class CompetenciaHistoricaDirectaStrategy implements CuotaStrategy {
 	
 	//@Override
-	
+	public float calcularProbabilidadDeGanar(HistorialDelPartido historial){
+		
+	return historial.getGanoOponenteUno / historial.getPartidosJugados();
+	}
 
 }
